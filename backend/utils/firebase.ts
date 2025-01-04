9481aadf36d373bde75e158126d17f9b80944163
TeddyMuli <tmuli974@gmail.com>
2025-01-04 23:09:40 +0300

    set up project

---
+++ backend/utils/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+  apiKey: "AIzaSyBGKqXLIUf1PGUSNeCcOe4ri7Fu7j8l9xA",
+  authDomain: "freelance-app-58893.firebaseapp.com",
+  projectId: "freelance-app-58893",
+  storageBucket: "freelance-app-58893.firebasestorage.app",
+  messagingSenderId: "674856384826",
+  appId: "1:674856384826:web:62880d2150fd3b16d73803",
+  measurementId: "G-K62VLK1ZR7"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
