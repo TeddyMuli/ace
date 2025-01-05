5395dca15021a8edf16f4a831e92a1c5d77ccf68
TeddyMuli <tmuli974@gmail.com>
2025-01-05 19:04:46 +0300

    Add firebase auth

--- backend/utils/firebase.ts
+++ backend/utils/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
@@ -18,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const auth = getAuth(app);
