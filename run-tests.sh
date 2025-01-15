a2e187547f1c66a712d37e68ba17605d2cb49a17
TeddyMuli <tmuli974@gmail.com>
2025-01-15 15:12:05 +0300

    modify run-tests.sh to test auth

--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "Running auth tests..."
-npx jest tests/backend/mutations/auth.test.ts --detectOpenHandles
+npx jest tests/backend/auth.test.ts --detectOpenHandles
 if [ $? -ne 0 ]; then
   echo "Auth tests failed"
   exit 1
 fi
 
 echo "Running mutation tests..."
-npx jest tests/backend/mutations/mutations.test.ts --detectOpenHandles
+npx jest tests/backend/mutations.test.ts --detectOpenHandles
 if [ $? -ne 0 ]; then
   echo "Mutation tests failed"
   exit 1
 fi
 
 echo "Running query tests..."
-npx jest tests/backend/queries/queries.test.ts --detectOpenHandles
+npx jest tests/backend/queries.test.ts --detectOpenHandles
 if [ $? -ne 0 ]; then
   echo "Query tests failed"
   exit 1
