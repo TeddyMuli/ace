a791a1442c40a6c16ac81f6280f8af7215d6f8d0
TeddyMuli <tmuli974@gmail.com>
2025-01-15 15:11:23 +0300

    modify run-tests.sh to test auth

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Running auth tests..."
+npx jest tests/backend/mutations/auth.test.ts --detectOpenHandles
+if [ $? -ne 0 ]; then
+  echo "Auth tests failed"
+  exit 1
+fi
+
 echo "Running mutation tests..."
 npx jest tests/backend/mutations/mutations.test.ts --detectOpenHandles
 if [ $? -ne 0 ]; then
